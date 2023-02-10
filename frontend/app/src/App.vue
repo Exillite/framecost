3e61bd656c88e966a4a116f8fe63fc72577c8989
Exillite <greencod67@gmail.com>
2023-02-10 20:59:02 +0300

    add vue to project

---
+++ frontend/app/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="">Hi!!</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: 'Hello Vue!'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
