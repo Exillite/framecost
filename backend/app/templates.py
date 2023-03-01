02ee5c8660299c2cc5808ff628a85d915d59c8d3
Exillite <greencod67@gmail.com>
2023-03-01 11:38:11 +0300

    add teplate Rama_With_Cant

--- backend/app/templates.py
+++ backend/app/templates.py
@@ -89,3 +89,38 @@ class With_Paspartu(BaseTemplate):
         self.steklo = (self.width + 4 + self.paspartu_width * 2) * (self.height + 4 + self.paspartu_width * 2)
         self.natazka = (self.width + self.width) * 2
         self.tros = 20 + self.width + self.paspartu_width * 2
+
+
+class Rama_With_Cant(BaseTemplate):
+    rama: float
+    osnova: float
+    zadnik: float
+    steklo: float
+    natazka: float
+    cant: float
+    tros: float
+    
+    def __init__(self, width: float, height: float, is_horisontal: bool, baget_width: float):
+        self.is_horisontal = is_horisontal
+        self.width = width
+        self.height = height
+        if self.is_horisontal and self.width < self.height:
+            self.width, self.height = self.height, self.width
+            
+        if not self.is_horisontal and self.width > self.height:
+            self.width, self.height = self.height, self.width
+            
+        self.baget_width = baget_width
+        
+        self.calculate()
+        
+    def calculate(self):
+        self.rama = ((self.width + 1 + 3.6 + self.baget_width * 2) + (self.height + 1 + 3.6 + self.baget_width * 2)) * 2
+        self.osnova = (self.width + 2) * (self.height + 2)
+        self.zadnik = (self.width + 2 + 4 + 4) * (self.height + 2 + 4 + 4)
+        self.steklo = (self.width + 2 + 3.6) * (self.height + 2 + 3.6)
+        self.natazka = (self.width + self.width) * 2
+        self.cant = (self.width + 3 + 3.6 + self.height + 3 + 3.6) * 2
+        self.tros = 20 + self.width + 4 * 2
+
+        
