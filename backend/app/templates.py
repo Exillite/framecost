0f09a96f8395191d499e78d85b25920601d99a25
Exillite <greencod67@gmail.com>
2023-03-02 22:13:26 +0300

    Double_Rama_With_Paspartu

--- backend/app/templates.py
+++ backend/app/templates.py
@@ -187,3 +187,47 @@ class Rama_With_Natazka_Na_Podramnik(BaseTemplate):
         self.podramnic60 = (self.width + 2 + self.height + 2) * 2 + self.less_storona
         self.tros = 20 + self.width
         self.steklo = (self.width + 4) * (self.height + 4)
+
+
+class Double_Rama_With_Paspartu(BaseTemplate):
+    rama1: float
+    rama2: float
+    paspartu: float
+    nakleyka: float
+    dvoynoe: float
+    troynoe: float
+    osnova: float
+    zadnik: float
+    steklo: float
+    natazka: float
+    tros: float
+    
+    def __init__(self, width: float, height: float, is_horisontal: bool, baget1_width: float, baget2_width: float, paspartu_width: float):
+        self.is_horisontal = is_horisontal
+        self.width = width
+        self.height = height
+        if self.is_horisontal and self.width < self.height:
+            self.width, self.height = self.height, self.width
+            
+        if not self.is_horisontal and self.width > self.height:
+            self.width, self.height = self.height, self.width
+            
+        self.baget1_width = baget1_width
+        self.baget2_width = baget2_width
+        
+        self.paspartu_width = paspartu_width
+        
+        self.calculate()
+        
+    def calculate(self):
+        self.rama1 = ((self.width + 1.5 + self.baget1_width * 2 + self.paspartu_width * 2) + (self.height + 1.5 + self.baget1_width * 2 + self.paspartu_width * 2)) * 2
+        self.rama2 = (self.width + 4 + self.paspartu_width * 2 + self.baget1_width * 2 + self.baget2_width * 2) * 2 + (self.height + 4 + self.paspartu_width * 2 + self.baget1_width * 2 + self.baget2_width * 2) * 2
+        self.paspartu = (self.width + 2 + self.paspartu_width * 2) * (self.height + 2 + self.paspartu_width * 2)
+        self.nakleyka = ((self.width + 2) + (self.height + 2)) * 2
+        self.dvoynoe = ((self.width + 2) + (self.height + 2)) * 2
+        self.troynoe = ((self.width + 2) + (self.height + 2)) * 2 + ((self.width + 3) + (self.height + 3))
+        self.osnova = (self.width + 2) * (self.height + 2)
+        self.zadnik = (self.width + 2 + self.paspartu_width + self.paspartu_width + self.baget1_width + self.baget1_width + 3) * (self.height + 2 + self.paspartu_width + self.paspartu_width + self.baget1_width + self.baget1_width + 3)
+        self.steklo = (self.width + 4 + self.paspartu_width * 2 + self.baget1_width * 2) * (self.height + 4 + self.paspartu_width * 2 + self.baget1_width * 2)
+        self.natazka = (self.width + self.height) * 2
+        self.tros = 20 + self.width + self.paspartu_width * 2 + self.baget1_width * 2
