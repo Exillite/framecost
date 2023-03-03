0e8d7b0e1ad37bc8bae1f213aee0824db801d971
Exillite <greencod67@gmail.com>
2023-03-03 19:08:46 +0300

    add template Rama_With_Cant_And_Double_Paspartu

--- backend/app/templates.py
+++ backend/app/templates.py
@@ -269,3 +269,43 @@ class Rama_With_Cant_And_Single_Paspartu(BaseTemplate):
         self.steklo = (self.width + 4 + self.paspartu_width * 2 + self.baget_width * 2) * (self.height + 4 + self.paspartu_width * 2 + self.baget_width * 2)
         self.natazka = (self.width + self.height) * 2
         self.tros = 20 + self.width + self.paspartu_width * 2 + self.baget_width * 2
+
+
+class Rama_With_Cant_And_Double_Paspartu(BaseTemplate):
+    rama: float
+    paspartu: float
+    pricleyka: float
+    dvoynoe: float
+    cant: float
+    osnova: float
+    zadnik: float
+    steklo: float
+    natazka: float
+    tros: float
+    
+    def __init__(self, width: float, height: float, is_horisontal: bool, baget_width: float, paspartu_width: float):
+        self.is_horisontal = is_horisontal
+        self.width = width
+        self.height = height
+        if self.is_horisontal and self.width < self.height:
+            self.width, self.height = self.height, self.width
+            
+        if not self.is_horisontal and self.width > self.height:
+            self.width, self.height = self.height, self.width
+            
+        self.baget_width = baget_width
+        self.paspartu_width = paspartu_width
+        
+        self.calculate()
+    
+    def calculate(self):
+        self.rama = (self.width + 1.5 + self.baget_width * 2 + self.paspartu_width * 2) * 2 + (self.height + 1.5 + self.baget_width * 2 + self.paspartu_width * 2) * 2
+        self.paspartu = (self.width + 2 + self.paspartu_width * 2) * (self.height + 2 + self.paspartu_width * 2)
+        self.pricleyka = ((self.width + 3) + (self.height + 3)) * 2
+        self.dvoynoe = ((self.width + 3) + (self.height + 3)) * 2
+        self.cant = (self.width + 5) * 2 + (self.height + 5) * 2
+        self.osnova = (self.width + 2) * (self.height + 2)
+        self.zadnik = (self.width + 2 + self.paspartu_width * 2 + self.baget_width * 2) * (self.height + 2 +  self.paspartu_width * 2 + self.baget_width * 2)
+        self.steklo = (self.width + 4 + self.paspartu_width * 2) * (self.height + 4 + self.paspartu_width * 2)
+        self.natazka = (self.width + self.height) * 2
+        self.tros = 20 + self.width + self.paspartu_width * 2 + 3
